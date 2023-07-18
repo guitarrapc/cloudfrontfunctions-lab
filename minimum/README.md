a2c26408db79a7c780feb0771f2c2cccc85a89b9
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-07-18 16:54:12 +0900

    feat: add minimum

---
+++ minimum/README.md
@@ -0,0 +1,5 @@
+## minimum
+
+Always redirect to https://aws.amazon.com/cloudfront/
+
+> **Note** This example function is for a viewer request event trigger. Choose viewer request for event trigger when you associate this function with a distribution.
---
+++ minimum/minimum.js
@@ -0,0 +1,11 @@
+function handler(event) {
+  var response = {
+    statusCode: 302,
+    statusDescription: 'Found',
+    headers: {
+      'cloudfront-functions': { value: 'generated-by-CloudFront-Functions' },
+      location: { value: 'https://aws.amazon.com/cloudfront/' },
+    },
+  }
+  return response
+}
